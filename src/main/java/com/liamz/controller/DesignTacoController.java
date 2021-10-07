2a2c6a4eeb6694311eb9c34b57570f85f918f59d
liam <zhuchanglizq@126.com>
2021-10-07 22:39:37 +0800

    增加校验信息

--- src/main/java/com/liamz/controller/DesignTacoController.java
+++ src/main/java/com/liamz/controller/DesignTacoController.java
@@ -5,10 +5,13 @@ import com.liamz.entites.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -58,7 +61,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design){
+    public String processDesign(
+            @Valid @ModelAttribute("design") Taco design, Errors errors){
+        if(errors.hasErrors()){
+            return "design";
+        }
         log.info("Process design: "+ design);
         return "redirect:/orders/current";
     }
--- src/main/java/com/liamz/entites/Order.java
+++ src/main/java/com/liamz/entites/Order.java
@@ -1,6 +1,11 @@
 package com.liamz.entites;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 /**
  * @Description
@@ -9,12 +14,21 @@ import lombok.Data;
  **/
 @Data
 public class Order {
+
+    @NotBlank(message = "Name is Required")
     private String name;
+    @NotBlank(message = "street is required")
     private String Street;
+    @NotBlank(message = "city is required")
     private String city;
+    @NotBlank(message = "state is required")
     private String state;
+    @NotBlank(message = "zip is required")
     private String zip;
+    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|[0-2])([\\/])([1-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
+    @Digits(integer = 3,fraction = 0,message = "Invalid CVV")
     private String ccCVV;
 }
